1a560476762abefb6ed2b32f55dd5fbfadafbec2
Hzirhtjul <Hzirhtjul>
2022-11-08 12:05:25 +0800

    MyFirstApp

--- app/src/main/java/com/example/myfirstapp/FirstActivity.java
+++ app/src/main/java/com/example/myfirstapp/FirstActivity.java
@@ -2,33 +2,97 @@ package com.example.myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.regex.Pattern;
 
 public class FirstActivity extends AppCompatActivity {
 
     EditText etName;
+    EditText etAddress;
+    EditText etEmail;
+    EditText etNoPhone;
+    EditText etBirthdayDate;
     Button btnName;
-    public final static String MESSAGE_KEY ="com.example.MyFirstApp.message_key";
+
+    public final static String USERNAME = "com.example.MyFirstApp.message_key";
+    public final static String ADDRESS = "com.example.MyFirstApp.ADDRESS";
+    public final static String EMAIL = "com.example.MyFirstApp.EMAIL";
+    public final static String NUMBERPHONE = "com.example.MyFirstApp.NUMBERPHONE";
+    public final static String BIRTHDATE = "com.example.MyFirstApp.BIRTHDATE";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+
+        etBirthdayDate = (EditText) findViewById(R.id.etBirthdayDate);
+        etBirthdayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar c = Calendar.getInstance();
+                int mYear = c.get(Calendar.YEAR);
+                int mMonth = c.get(Calendar.MONTH);
+                int mDay = c.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(FirstActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                etBirthdayDate.setText(dayOfMonth + "/"
+                                        + (monthOfYear + 1) + "/" + year);
+                            }
+                        }, mYear, mMonth, mDay);
+                datePickerDialog.show();
+
+            }
+        });
+
     }
 
-    public void send_data(View view){
-        etName=findViewById(R.id.etName);
-        btnName=findViewById(R.id.btnName);
-        String message = etName.getText().toString();
-        Intent intent =new Intent(FirstActivity.this,SecondActivity.class);
-        intent.putExtra(MESSAGE_KEY,message);
-        startActivity(intent);
+    public void send_data(View view) {
+        etName = findViewById(R.id.etName);
+        etEmail=findViewById(R.id.etEmail);
+        etAddress = findViewById(R.id.etAddress);
+        etNoPhone = findViewById(R.id.etNoPhone);
+        etBirthdayDate = findViewById(R.id.etBirthdayDate);
+        btnName = findViewById(R.id.btnName);
 
+        String Username = etName.getText().toString();
+        String Email= etEmail.getText().toString();
+        String Birthdate = etBirthdayDate.getText().toString();
+        String Address = etAddress.getText().toString();
+        String NoPhone = etNoPhone.getText().toString();
 
+        if(!Email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(Email).matches()){
 
+            Toast.makeText(this,"You are register now!!", Toast.LENGTH_SHORT).show();
+
+            Intent intent = new Intent(FirstActivity.this,SecondActivity.class);//refer this
+            intent.putExtra(USERNAME,Username);
+            intent.putExtra(EMAIL,Email);
+            intent.putExtra(BIRTHDATE,Birthdate);
+            intent.putExtra(ADDRESS,Address);
+            intent.putExtra(NUMBERPHONE,NoPhone);
+            startActivity(intent);
+
+        }
+        //If the email format is wrong, it will be display Invalid Email Address!
+        else{
+
+            Toast.makeText(this,"Invalid Email!",Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/example/myfirstapp/SecondActivity.java
+++ app/src/main/java/com/example/myfirstapp/SecondActivity.java
@@ -1,26 +1,57 @@
 package com.example.myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
-    public final static String MESSAGE_KEY = "com.example.MyFirstApp.message_key";
+    public final static String USERNAME= "com.example.MyFirstApp.message_key";
+    public final static String EMAIL = "com.example.MyFirstApp.EMAIL";
+    public final static String BIRTHDATE = "com.example.MyFirstApp.BIRTHDATE";
+    public final static String ADDRESS="com.example.MyFirstApp.ADDRESS";
+    public final static String NUMBERPHONE = "com.example.MyFirstApp.NUMBERPHONE";
+
+
+
+    TextView tvUserName,tvEmail,tvBirthday,tvAddress,tvNoPhone;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_second);
+
+        tvUserName=findViewById(R.id.tvUserName);
+        tvEmail=findViewById(R.id.tvEmail);
+        tvBirthday=findViewById(R.id.tvBirthday);
+        tvAddress=findViewById(R.id.tvAddress);
+        tvNoPhone=findViewById(R.id.tvNoPhone);
 
         Intent intent = getIntent();
-        String message = intent.getStringExtra(MESSAGE_KEY);
-        TextView textView = new TextView(this);
-        textView.setTextSize(45);
-        textView.setText(message);
-        setContentView(textView);
 
+        String Username =intent.getStringExtra(USERNAME);
+        String Email = intent.getStringExtra(EMAIL);
+        String Birthdate=intent.getStringExtra(BIRTHDATE);
+        String Address=intent.getStringExtra(ADDRESS);
+        String NoPhone=intent.getStringExtra(NUMBERPHONE);
+
+
+        tvUserName.setTextSize(20);
+        tvUserName.setText("Name : "+Username);
+
+        tvEmail.setTextSize(20);
+        tvEmail.setText("\nEmail : "+Email);
+
+        tvBirthday.setTextSize(20);
+        tvBirthday.setText("\nBirth Date : "+Birthdate);
+
+        tvAddress.setTextSize(20);
+        tvAddress.setText("\nAddress : "+Address);
+
+        tvNoPhone.setTextSize(20);
+        tvNoPhone.setText("\nNo Phone: "+NoPhone);
+        
     }
 }
